c5b1e7fe1853def626cb21c231b18f3eb293122d
JJunee <wnsltkak@naver.com>
2022-11-20 22:28:49 +0900

    1day

---
+++ 1day.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int a, b;
+	int result;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	result = a + b;
+	printf("%d + %d = %d\n", a, b, result);
+
+	return 0;
+
+
+}
